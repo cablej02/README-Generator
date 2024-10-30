cf763360fbe5992726dd5447bef0ea2b2bf6dcf2
Jason Cable <cablej02@gmail.com>
2024-10-30 18:31:39 -0500

    added imports, questions, and generateMD function

--- index.js
+++ index.js
@@ -1,13 +1,96 @@
-// TODO: Include packages needed for this application
+import {writeFile} from 'fs/promises';
+import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const askQuestions = () =>{
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter the title of your project: '
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description of your project: '
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter installation instructions for your project: '
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter usage information for your project: '
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: 'Enter contribution guidelines for your project: '
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Enter test instructions for your project: '
+        },
+        {
+            type: 'list',
+            name: 'liscense',
+            message: 'Choose a liscense for your project: ',
+            choices: ['none','MIT Liscense','GNU GPLv3','GNU AGPLv3','GNU LGPLv3','Mozilla Public License 2.0','Apache License 2.0','Boost Software Liscense 1.0','The Unlicense']
+        }
+    ])
+}
+
+//TODO: verify table of contents is working
+const generateMD = ({title,description,installation,usage,liscense,contribute,tests, github, email}) =>{
+    return `# ${title}
+
+## Description
+
+${description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## License
+
+${liscense}
+
+## Contributing
+
+${contribute}
+
+## Tests
+
+${tests}
+
+## Questions
+
+GitHub: ${github}
+If you have additional questions, please contact me by email at ${email}`
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {}
 
 // Function call to initialize app
 init();
